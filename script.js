11cef399218b235e4474315b6ed18852a1643a68
borgogaby <159448525+borgogaby@users.noreply.github.com>
2024-07-04 10:36:06 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado:"pergunta1",
+        alternativas: ["alternativa1", "alternativa2"]
+    },
+    { enunciado:"pergunta2",
+      alternativas:["alternativas1", "alternativa2"]
+    }
+];
